553efe341da1831821e1dccc79ca9dac71c9deb5
maru-star <maruoka@web-trickster.com>
2022-07-07 16:08:41 +0900

    レイアウトを挿入

--- template.php
+++ template.php
@@ -1,12 +1,95 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
+
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Vnet リニューアル共通パーツ</title>
+  <meta name="format-detection" content="telephone=no">
+  <link rel="canonical" href="">
+  <!-- OGP -->
+  <meta name="description" content="">
+  <meta property="og:title" content="">
+  <meta property="og:description" content="">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+  <meta property="og:site_name" content="">
+  <!-- favicon/icon -->
+  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+  <link rel="apple-touch-icon" href="ogp.jpg">
+  <link rel="icon" type="image/png" href="">
+
+  <!-- CSS -->
+  <link href="/assets/css/style.css" rel="stylesheet">
+
+  <!-- JAVASCRIPT -->
+
+
+  <title>Vnetリニューアルテンプレート</title>
 </head>
+
 <body>
-  
+  <div class="l-global-container">
+    <header class="l-header">
+      <div class="l-header__container">
+        <div class="l-header__logo"><a href="/"><img src="/assets/img/common/logo.png" alt=""></a></div>
+        <button class="l-header__btn" id="js-header-button">
+          <div class="box">
+            <span></span><span></span><span></span>
+          </div>
+        </button>
+        <nav class="l-header__pcNav">
+          <ul class="l-header__pcNav__list">
+            <li style="width:20%;"></li>
+            <li><a href="#intro">当校が考える、受験のポイント<span class="border">合格を目指す<br>皆さんへ</span></a></li>
+            <li><a href="#strategy">受験の挑み方がある！<span class="border middle">受験「攻略法」</span></a></li>
+            <li><a href="#support">大学も応援しています<span class="border middle">協賛大学</span></a></li>
+            <li><a href="#faq">受験のノウハウ<span class="border middle">一問一答！</span></a></li>
+          </ul>
+        </nav>
+      </div>
+      <nav class="l-nav" id="js-nav">
+        <div class="l-nav__container">
+          <ul class="l-nav__list">
+            <li class="l-nav__item"><a href="#intro">当校が考える、受験のポイント<span>合格を目指す皆さんへ</span></a></li>
+            <li class="l-nav__item"><a href="#strategy">受験の挑み方がある！<span>受験「攻略法」</span></a></li>
+            <li class="l-nav__item"><a href="#support">大学も応援しています<span>協賛大学</span></a></li>
+            <li class="l-nav__item"><a href="#faq">受験のノウハウ<span>一問一答！</span></a></li>
+          </ul>
+          <div class="l-nav__btn">
+            <a href="https://www.yuhigaoka.ac.jp/form/consultation" target="_blank" rel="noopener noreferrer" class="c-link-btn c-link-btn--contact">お問い合わせ</a>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <nav class="l-fixed-nav" id="js-side-nav">
+      <a href="https://www.yuhigaoka.ac.jp/form/consultation" target="_blank" rel="noopener noreferrer"><span>お問い合わせ</span></a>
+    </nav>
+    <main class="l-main">
+      <div class="p-home-hero">
+        <div class="p-home-hero__img"></div>
+        <div class="p-home-hero__container">
+          <p class="p-home-hero__text"></p>
+          <div class="p-home-hero__box">
+            <div class="p-home-hero__catch">
+            </div>
+          </div>
+        </div>
+      </div>
+      <section class="p-home-intro" id="intro">
+        <div class="l-container">
+          <h2 class="p-home-intro__title"><span class="inner"><img src="./assets/img/home/intro_title.svg" alt="夕陽丘予備校が考える、大学受験のポイント"><span>志望校合格を目指す皆さんへ</span></span></h2>
+          <p class="p-home-intro__text">大学進学を考えるにあたり、志望校が決まらない、何から始めればいいかわからない、学力に不安がある、失敗したらどうしよう…というのは当たり前のことです。<br> だからこそ、<span>一番の近道は「知っている人に聞くこと」</span>です。<br> 当校は70年の歴史のなかで、膨大な入試情報を蓄積し、数多くの生徒を送り出してきました。いわば、<span>「受験のプロ」</span>なのです。<br> 大学とのつながりを活かし、最新の傾向と対策、受験方法に関する情報を収集。<br> それをもとに、受験生が自信を持って入試に臨めるよう一人ひとりにフォーカスしたアドバイス行っています。<br> 一番身近にいる私たちの「知識」、「経験」をめいっぱいに活用してください！</p>
+          <p class="p-home-intro__catch">まずは私たちと一緒に<br class="u-pc-less"><span>受験へ挑む準備</span>、<br><span>その流れ</span>を<br class="u-pc-less">見ていきましょう！</p>
+        </div>
+        <a href="#strategy" class="p-home-intro__btn"></a>
+      </section>
+    </main>
+    <footer class="l-footer">
+    </footer>
+  </div>
 </body>
+
 </html>
